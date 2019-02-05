8da474c4a30b8eb978b2a3f822b44fc23a223375
Jacob Lindow <40470940+jlindow@users.noreply.github.com>
2019-02-05 09:00:13 -0500

    Update server.c

--- server.c
+++ server.c
@@ -24,46 +24,50 @@ char buffer[BUF_LEN];
 
 int main(int argc, char *argv[])
 {
-int sockfd_listen;
-int sockfd_connect;
-int port;
+    int sockfd_listen;
+    int sockfd_connect;
+    int port;
 
-struct sockaddr_in address;     // Stores IP Address of Server
-int length = sizeof(address);
+    struct sockaddr_in address;     // Stores IP Address of Server
+    int length = sizeof(address);
 
-sockfd_listen = socket(AF_INET, SOCK_STREAM, 0);
-if(sockfd_listen < 0){
+    sockfd_listen = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockfd_listen < 0)
+    {
         perror("Error:");
-}
+    }
 
-bzero(&address, sizeof(address));
+    bzero(&address, sizeof(address));
 
-port = atoi(argv[1]);
-address.sin_family = AF_INET;
-address.sin_addr.s_addr = htons(INADDR_ANY);
-address.sin_port = htons(port);
+    port = atoi(argv[1]);
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = htons(INADDR_ANY);
+    address.sin_port = htons(port);
 
-if(bind(sockfd_listen, (struct sockaddr *)&address, sizeof(address)) < 0){
+    if(bind(sockfd_listen, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
         perror("Error:");
-}
+    }
 
-if(listen(sockfd_listen, 10) < 0){
+    if(listen(sockfd_listen, 10) < 0)
+    {
         perror("Error:");
-}
+    }
 
-if(sockfd_connect = accept(sockfd_listen, (struct sockaddr *) NULL, NULL) < 0){
-                perror("Error:");
+    if(sockfd_connect = accept(sockfd_listen, (struct sockaddr *) NULL, NULL) < 0)
+    {
+        perror("Error:");
+    }
+    
+    while(1) 
+    {
+        int length = read(sockfd_connect, buffer, BUF_LEN); 
+        write(1, buffer, length); 
 
-        }
-while(1) {
-int length = read(sockfd_connect, buffer, BUF_LEN); 
-write(1, buffer, length); 
+        length = read(0, buffer, BUF_LEN); 
+        write(sockfd_connect, buffer, length);
+    }
 
-length = read(0, buffer, BUF_LEN); 
-write(sockfd_connect, buffer, length);
-
-}
-
-close(sockfd_listen);
-close(sockfd_connect);
+    close(sockfd_listen);
+    close(sockfd_connect);
 }
